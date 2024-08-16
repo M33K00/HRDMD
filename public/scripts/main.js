4203e94c3637b8a6429de5221af5a4a8ba4b0803
M33K00 <mikuosuzuya@gmail.com>
2024-08-16 14:34:34 +0800

    1.77

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,14 +1,17 @@
-const { app, BrowserWindow, session } = require("electron");
+const { app, BrowserWindow, session, Tray, Menu } = require("electron");
 const server = require("./index");
 const path = require("path");
 const PORT = 3939;
+
+let mainWindow;
+let tray;
 
 function createWindow() {
   const customSession = session.fromPartition("persist:my-cache-session", {
     cache: true,
   });
 
-  const win = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     maxHeight: 1080,
     maxWidth: 1920,
     width: 1600,
@@ -24,13 +27,50 @@ function createWindow() {
     },
   });
 
-  win.setMinimumSize(1600, 900);
+  mainWindow.setMinimumSize(1600, 900);
+  mainWindow.loadURL(`http://localhost:${PORT}`);
 
-  win.loadURL(`http://localhost:${PORT}`);
+  // Minimize to tray on close
+  mainWindow.on("close", function (event) {
+    if (!app.isQuiting) {
+      event.preventDefault();
+      mainWindow.hide();
+    }
+  });
+}
+
+function createTray() {
+  tray = new Tray(path.join(__dirname, "../images/logo.ico")); // Replace with your tray icon
+  const contextMenu = Menu.buildFromTemplate([
+    {
+      label: "Show App",
+      click: function () {
+        mainWindow.show();
+      },
+    },
+    {
+      label: "Quit",
+      click: function () {
+        app.isQuiting = true;
+        app.quit();
+      },
+    },
+  ]);
+  tray.setToolTip("Your Electron App");
+  tray.setContextMenu(contextMenu);
+
+  tray.on("click", () => {
+    if (mainWindow.isVisible()) {
+      mainWindow.hide();
+    } else {
+      mainWindow.show();
+    }
+  });
 }
 
 app.whenReady().then(() => {
   createWindow();
+  createTray();
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) {
